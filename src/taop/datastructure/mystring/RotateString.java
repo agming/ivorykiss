0723c15a5de40eb6f034b6ffc0d3a4ea9f4eecbd
Daohao <haoming.ghm@alibaba-inc.com>
2015-01-03 17:08:24 +0800

    my first build

--- src/taop/datastructure/mystring/RotateString.java
+++ src/taop/datastructure/mystring/RotateString.java
@@ -3,8 +3,27 @@ package taop.datastructure.mystring;
 public class RotateString {
 
 	public static void main(String[] args) {
-
+		RotateString rotateString = new RotateString();
+		String str = "abcdef";
+		rotateString.leftRotateString(str.toCharArray(), 6, 2);
+		
 	}
 	
+	void leftShiftOne(char[] arr, int n) {
+		char c = arr[0];
+		for(int i = 1; i < n; i++) {
+			arr[i - 1] = arr[i];
+		}
+		arr[n - 1] = c;
+	}
+	
+	void leftRotateString(char[] arr, int n, int m) {
+		while(m > 0) {
+			leftShiftOne(arr, n);
+			m--;
+		}
+		System.out.println(arr);
+	}
 
 }
+
